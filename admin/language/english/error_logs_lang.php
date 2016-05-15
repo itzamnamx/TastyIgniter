fab4926b38500f663f62cf55f88ef518d8b894e9
victor <victor.tuxie@gmail.com>
2016-05-14 21:01:38 -0500

    Se traduce el Registro de errores del sistema

--- admin/language/english/error_logs_lang.php
+++ admin/language/english/error_logs_lang.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['text_title'] 		            = 'Error Logs';
-$lang['text_heading'] 		            = 'Error Logs';
+$lang['text_title'] 		            = 'Registro de Errores del Sistema';
+$lang['text_heading'] 		            = 'Registro de Errores del Sistema';
 
 /* End of file error_logs_lang.php */
 /* Location: ./admin/language/english/error_logs_lang.php */
